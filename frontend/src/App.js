bbe50bb847f940a28c63d6902e0f6807844b0f2d
RahulML2505GitHub <rahulml2505ytc@gmail.com>
2021-11-09 11:41:19 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App(props) {
+    return (
+      <>
+        <Navbar title="Ice-Cream Shop"/>
+        <div className="container">
+          <p> I'm Rahul from India. </p>
+        </div>
+      </>
+    );
+}
+
+export default App;
